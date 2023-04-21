294e23fa57f0a8d58eb91244fd11938bf29d75f6
fanxil123 <105046957+fanxil123@users.noreply.github.com>
2023-04-21 16:38:44 +0800

    Initial commit

---
+++ react/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    port: 3000
+  }
+};
